7559a1b9847a1998ed02a5cc6446682fe939f5d8
yattom <tsutomu.yasui@gmail.com>
2015-05-04 11:37:04 +0900

    add group function

--- static/scripts/gyomuflow.js
+++ static/scripts/gyomuflow.js
@@ -7,6 +7,11 @@ var drawrect = {
   currentRectIdSeq: 0,
 
   createRect: function(ev) {
+    this.createBaseRect(ev);
+    $('#rect_guide').css('left', ev.pageX);
+    $('#rect_guide').css('top', ev.pageY);
+  },
+  createBaseRect: function(ev) {
     this.currentRectIdSeq += 1;
     this.startX = ev.pageX;
     this.startY = ev.pageY;
@@ -17,30 +22,105 @@ var drawrect = {
     $('#' + rectId).click(drawline.rectClick);
 
     $('#rect_guide').show();
-    $('#rect_guide').css('left', ev.pageX);
-    $('#rect_guide').css('top', ev.pageY);
+    return rectId;
   },
-  resizeRect: function(ev) {
+  removeRect: function(rectId) {
+    var i;
+    var lineIdToRemove = [];
+    for(i = 0; i < $('.line').length; i++) {
+      var id = $('.line')[i].id;
+      if (rectId  == $('#' + id).data('fromRectId')
+       || rectId  == $('#' + id).data('toRectId')) {
+        lineIdToRemove.push(id);
+      }
+    }
+    for(i = 0; i < lineIdToRemove.length; i++) {
+      $('#' + lineIdToRemove[i]).remove();
+    }
+
+    for(i = 0; i < $('.rect').length; i++) {
+      var el = $($('.rect')[i]);
+      if(!el.data('groupMember')) {
+        continue;
+      }
+      var idx = el.data('groupMember').indexOf(rectId);
+      if(idx >= 0) { el.data('groupMember').splice(idx, 1); }
+      drawrect.resizeGroup(el[0].id);
+    }
+    $('#' + rectId).remove();
+  },
+  resizeRect: function(cursorX, cursorY) {
     var rectId = 'rect_' + this.currentRectIdSeq;
-    var l = Math.min(this.startX, ev.pageX)
-    var w = Math.abs(this.startX - ev.pageX)
-    var t = Math.min(this.startY, ev.pageY)
-    var h = Math.abs(this.startY - ev.pageY)
+    var l = Math.min(this.startX, cursorX)
+    var w = Math.abs(this.startX - cursorX)
+    var t = Math.min(this.startY, cursorY)
+    var h = Math.abs(this.startY - cursorY)
     $('#' + rectId).css('left', l);
     $('#' + rectId).css('top', t);
     $('#' + rectId).width(w)
     $('#' + rectId).height(h);
     $('#' + rectId).height(h);
 
-    var d = this.startX < ev.pageX ? (this.startY < ev.pageY ? 'se' : 'ne') : (this.startY < ev.pageY ? 'sw' : 'nw');
+    var d = this.startX < cursorX ? (this.startY < cursorY ? 'se' : 'ne') : (this.startY < cursorY ? 'sw' : 'nw');
     $('#' + rectId).toggleClass('ne', d == 'ne');
     $('#' + rectId).toggleClass('se', d == 'se');
     $('#' + rectId).toggleClass('sw', d == 'sw');
     $('#' + rectId).toggleClass('nw', d == 'nw');
 
-    $('#rect_guide').css('left', ev.pageX);
-    $('#rect_guide').css('top', ev.pageY);
+    $('#rect_guide').css('left', cursorX);
+    $('#rect_guide').css('top', cursorY);
   },
+  createGroupRect: function(ev) {
+    var rectId = drawrect.createBaseRect(ev);
+    $('#' + rectId).addClass('group');
+    $('#' + rectId).data('groupMember', []);
+    return rectId;
+  },
+  addToGroup: function(groupId, rectId) {
+    if($('#' + groupId).data('groupMember').indexOf(rectId) >= 0) {
+      return;
+    }
+    var rectIds = $('#' + groupId).data('groupMember');
+    rectIds.push(rectId);
+    this.resizeGroup(groupId);
+  },
+  resizeGroup: function(groupId) {
+    var i;
+    var l, r, t, b;
+    var rectIds = $('#' + groupId).data('groupMember');
+    for(i = 0; i < rectIds.length; i++) {
+      var target = $('#' + rectIds[i]);
+      var t_l = target.position().left;
+      var t_r = target.position().left + target.width();
+      var t_t = target.position().top;
+      var t_b = target.position().top + target.height();
+      if(!(l < t_l)) { l = t_l; }
+      if(!(r > t_r)) { r = t_r; }
+      if(!(t < t_t)) { t = t_t; }
+      if(!(b > t_b)) { b = t_b; }
+    }
+    margin = 8;
+    $('#' + groupId).css('left', l - margin);
+    $('#' + groupId).css('top', t - margin);
+    $('#' + groupId).width(r - l + margin * 2);
+    $('#' + groupId).height(b - t + margin * 2);
+  },
+  isInGroup: function(rectId) {
+    return drawrect.getGroupRectId(rectId) != null;
+  },
+  getGroupRectId: function(rectId) {
+    for(i = 0; i < $('.rect').length; i++) {
+      var el = $($('.rect')[i]);
+      if(!el.data('groupMember')) {
+        continue;
+      }
+      var idx = el.data('groupMember').indexOf(rectId);
+      if(idx >= 0) {
+        return el[0].id;
+      }
+    }
+    return null;
+  }
 };
 
 var drawline = {
@@ -68,6 +148,15 @@ var drawline = {
         $(this).removeClass('selected');
         drawline.selectedRectId = null;
         return false;
+    } else if(ev.shiftKey) {
+      if(drawrect.isInGroup(drawline.selectedRectId)) {
+        var groupRectId = drawrect.getGroupRectId(drawline.selectedRectId);
+        drawrect.addToGroup(groupRectId, ev.target.id);
+      } else {
+        var groupRectId = drawrect.createGroupRect(ev);
+        drawrect.addToGroup(groupRectId, drawline.selectedRectId);
+        drawrect.addToGroup(groupRectId, ev.target.id);
+      }
     } else {
       var lineId = 'line_' + drawline.currentLineIdSeq;
       var s = $('#' + drawline.selectedRectId);
@@ -120,11 +209,18 @@ $('body').mousemove(function(ev) {
   if(drawline.selectedRectId == null) {
     return true;
   }
-  var s = $('#' + drawline.selectedRectId);
-  var sX = s.position().left + s.width() / 2;
-  var sY = s.position().top + s.height() / 2;
-  var lineId = 'line_' + drawline.currentLineIdSeq;
-  drawline.lineTo(sX, sY, ev.pageX, ev.pageY, $('#' + lineId));
+  if(ev.shiftKey) {
+    var lineId = 'line_' + drawline.currentLineIdSeq;
+    $('#' + lineId).hide();
+  } else {
+    $('#' + drawline.selectedRectId).show();
+    var s = $('#' + drawline.selectedRectId);
+    var sX = s.position().left + s.width() / 2;
+    var sY = s.position().top + s.height() / 2;
+    var lineId = 'line_' + drawline.currentLineIdSeq;
+    drawline.lineTo(sX, sY, ev.pageX, ev.pageY, $('#' + lineId));
+    $('#' + lineId).show();
+  }
 });
 
 $('body').click(function(ev) {
@@ -141,18 +237,7 @@ $('body').click(function(ev) {
 $('body').keyup(function(ev) {
   if (drawline.selectedRectId != null) {
     if (ev.keyCode == 46) {
-      var toRemove = [];
-      for(i = 0; i < $('.line').length; i++) {
-        var id = $('.line')[i].id;
-        if (drawline.selectedRectId == $('#' + id).data('fromRectId')
-         || drawline.selectedRectId == $('#' + id).data('toRectId')) {
-          toRemove.push(id);
-        }
-      }
-      for(i = 0; i < toRemove.length; i++) {
-        $('#' + toRemove[i]).remove();
-      }
-      $('#' + drawline.selectedRectId).remove();
+      drawrect.removeRect(drawline.selectedRectId);
       drawline.selectedRectId = null;
     }
   }
@@ -165,7 +250,7 @@ $('body').mousemove(function(ev) {
   if (ev.button != 0) {
     return true;
   }
-  drawrect.resizeRect(ev);
+  drawrect.resizeRect(ev.pageX, ev.pageY);
   return false;
 });
 
